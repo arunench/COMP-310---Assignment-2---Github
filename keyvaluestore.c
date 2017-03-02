034317dfbfd122cc83132386390aa9760b5c0ba4
arunench <arunench@gmail.com>
2017-03-01 20:21:07 -0500

    Created memory object

--- keyvaluestore.c
+++ keyvaluestore.c
@@ -1,31 +1,31 @@
 //Arunen Chellan
 //Assignment 2
-#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int main (int argc, char **argv){
 
-static void *
-threadFunc(void *arg){
-	char *s = (char *) arg;
-	printf("%s", s);
-	return (void *) strlen(s);
-}
+	char *str = argv[1];
+	//Shared memory is created under the name "myshared"
+	int fd = shm_open("arunenshared", O_CREAT|O_RDWR, S_IRWXU); 
+	printf("Creating memory object...\n");
+	//Map the shared memory object starting at an address
+	//By specifying a NULL calue for the first argument of the mmap(), we are letting the kernel pick the starting location
+	//For the shared memory object in the virtual address space
+	char *addr = mmap(NULL, strlen(str), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
+	
+	//The ftruncatre() call is used to resize the shared memory object to fit the string (first argument)
+	ftruncate(fd, strlen(str));
 
-int main(int argc, char *argv[]){
-	pthread_t t1;
-	void *res;
-	int s;
+	close(fd);
+	
+	//We copy the bytes into the shared memory region
+	memcpy(addr, str, strlen(str));
 
-	s = pthread_create(&t1, NULL, threadFunc, "Hello world\n");
-	if (s != 0)
-		errExitEN(s, "pthread_create");
-
-	printf("Message from main()\n");
-	s = pthread_join(t1, &res);
-
-	if(s != 0)
-		errExitEN(s, "pthread_join");
-
-	printf("Thread returned %ld\n", (long) res);
-
-	exit(0);
 }
